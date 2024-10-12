b8660c1a5bf1fba29fa3dfe1666fe805deaaff81
Martin Orlej <xorlej@stuba.sk>
2024-10-12 19:10:20 +0200

    style: theme update

--- src/css/theme.ts
+++ src/css/theme.ts
@@ -1,11 +1,18 @@
-type Palette = {
-  primary: string
-  secondary: string
-  accent: string
-}
+const DEFAULT_SPACING = 4
 
-export const palette: Palette = {
+export const palette = {
   primary: '#0b192c',
   secondary: '#1e3e62',
-  accent: '#ff6500',
+  accent: '#EB8317',
+  background: '#1E201E',
+  textOnPrimary: '#ffffff',
+  error: '#ff0033',
+  border: 'rgba(255, 255, 255, 0.1)',
+  textOpaque: 'rgba(255, 255, 255, 0.4)',
+}
+
+export const spacing = (mult: number) => `${DEFAULT_SPACING * mult}px`
+
+export const containers = {
+  login: '460px',
 }
