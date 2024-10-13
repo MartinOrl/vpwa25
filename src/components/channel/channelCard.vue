6cc11566844d48af816e3b4beedd91fbfdbae00d
Martin Orlej <xorlej@stuba.sk>
2024-10-13 19:52:33 +0200

    feat: profile image popup

---
+++ src/components/channel/channelCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <img />
+    <div>
+      <p>
+        {{ $props.channel.name }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineComponent } from 'vue'
+import { ChannelInfo } from '@/utils/types/channel'
+
+defineProps<{
+  channel: ChannelInfo
+}>()
+
+defineComponent({
+  props: {
+    channel: {
+      type: Object as () => ChannelInfo,
+      required: true,
+    },
+  },
+})
+</script>
